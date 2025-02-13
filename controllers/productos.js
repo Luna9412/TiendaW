e23c3c76bce2ae12d7fc85f6ab2e1a900214a566
luna <jdluna9412@gmail.com>
2025-02-13 08:01:53 -0500

    consultas de productos

--- controllers/productos.js
+++ controllers/productos.js
@@ -1,11 +1,7 @@
 // controlador para el manejo de los productos
-
 //conectamos el controlador con su modelo correspondiente
-
 let producto = require('../models/productos')
-
 //toda la logica de un crud típico listartodos, listarpor id, crear, actualizar, borrar...
-
 const listartodos = async(req, res)=>{
 try {
     //consultar todos sin filtro
@@ -21,11 +17,9 @@ try {
     })
 }
 };
-
 //crear nuevo
 const nuevo = async (req, res) =>{
     // llega el objeto en el body del request
-
     let datos = {
         nombre: req.body.nombre,
         descripcion: req.body.descripcion,
@@ -46,25 +40,98 @@ const nuevo = async (req, res) =>{
         estado: true,
         mensaje: "¡Insercion Existosa!"
     });
-        
-
     }catch(error){
         return res.send ({
             estado:false,
             mensaje: `Error en la consulta ${error}`,
         });
     }
-    
-    
-
-
     // creamos el nuevo documento (qye agregaremos a la coleccion)
-
-
     //salvamos en mongo
 }
+// buscar por id o por otro parametros
+const buscarPorID = async (req, res)=>{
+    // recibimos le parametro por el cual debo buscar y eliminar
+    let id = req.params.id;
+try {
+    //logica de buscar y mostar el resultado del query
+    let consulta = await producto.findById(id).exec();
+    return res.send({
+        estado:true,
+        mensaje: "Busqueda existosa!",
+        consulta,
+    });
+} catch (error) { 
+    return res.send ({
+    estado:false,
+    mensaje: "¡Error, no fue posible encontrar el registro!",
+    consulta,
+})
+}
+};
+// actualizar de acuerdo al id del producto
+const actualizarPorID=async (req, res)=>{
+//recibe el parametro de la consulta
+let id = req.params.id;
 
+
+//payload que viene en el body :: los datos que manda el formulario
+let payload = {
+    nombre: req.body.nombre,
+    descripcion: req.body.descripcion,
+    imagen: req.body.imagen,
+    marca: req.body.marca,
+    precio: req.body.precio,
+    existencia: req.body.existencia,
+    rating: req.body.rating,
+    numRevisiones: req.body.numRevisiones,
+    estaOfertado: req.body.estaOfertado,
+};
+
+try {
+    let consulta = await producto.findByIdAndUpdate(id,payload).exec();
+    return res.send({
+        estado: true,
+        mensaje: "documento creado!",
+        consulta,
+    })
+} catch (error) {
+    return res.send({
+        estado: false,
+        mensaje: "error en la consulta!",
+        consulta,
+    })
+}
+
+};
+
+
+
+// borrar de acuerdo al id :::  RECUERDE QUE ESTE ES UN BORRADO DIDACTICO, NO USAR EN LA VIDA REAL
+const borrarPorID= async(req, res)=>{
+    // recibimos el parametro
+    let id = req.params.id;
+
+    try {
+        let consulta = await producto.findOneAndDelete({_id:id}).exec();
+        //let consulta= await producto.findByIdAndDelete(id).exec();
+        return res.send({
+            estado:true,
+            mensaje: "¡Borrado existoso!",
+            consulta,
+        });
+    } catch (error) {
+        return res.send({
+            estado:true,
+            mensaje: "¡Error en la consulta!",
+            consulta,
+        })
+    }
+}
 module.exports = {
     listartodos,
-    nuevo
+    nuevo,
+    buscarPorID,
+    borrarPorID,
+    actualizarPorID
 }
--- routes/productos.js
+++ routes/productos.js
@@ -9,8 +9,10 @@ const productoCtr = require('../controllers/productos')
 //rutas que entregará el modulo producto
 
 router.get("/producto/listartodos", productoCtr.listartodos);
-router.post("/producto/nuevo",productoCtr.nuevo)
-
+router.post("/producto/nuevo",productoCtr.nuevo);
+router.get("/producto/buscarPorID/:id",productoCtr.buscarPorID);
+router.delete("/producto/borrarPorID/:id",productoCtr.borrarPorID);
+router.put("/producto/actualizarPorID/:id",productoCtr.actualizarPorID);
 //......
 
 module.exports = router
