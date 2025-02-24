b8218859a0362b6ae0d3dddbfb5ee4417aa5fbf1
luna <jdluna9412@gmail.com>
2025-02-24 07:48:07 -0500

    creacion de tokens

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,7 @@
 //instanciamos la capa modelo correspondiente
+let bcrypt = require("bcryptjs")
 let Usuarios = require('../models/usuarios');
-
-
-
+let jwt = require("jsonwebtoken");
 // funciones de la libreria - metodos de la clase
 const listartodos = async(req, res)=>{
 try {
@@ -19,7 +18,86 @@ try {
     })
 }
 };
-
+const registro = async (req, res) => {
+    //recibir la data
+    let data = {
+        nombre: req.body.nombre,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        telefono: req.body.telefono,
+        esAdmin: req.body.esadmin,
+        direccion: req.body.direccion,
+        zip: req.body.zip,
+        ciudad: req.body.ciudad,
+        pais: req.body.pais,
+    };
+    let usuarioExiste = await Usuarios.findOne({ email: req.body.email }); 
+    if (usuarioExiste) {
+        return res.send({
+            estado: false,
+            mensaje: "el usuario ya esta registrado en el sistema",
+        });
+    }
+    try {
+    //objeto
+        let usuarioNuevo = new Usuarios(data);
+        usuarioNuevo.save();
+        res.send({
+            estado: true,
+            mensaje: "Usuario creado",
+        });
+        } catch (error) {
+            res.send({
+                estado: false,
+                mensaje: "¡Usuario no creado!",
+                error
+            });
+        }
+    };
+    //login tradicional : autenticacion de un factor
+    const login = async (req, res) => {
+    // recibir data: user / pass
+        let data = req.body.email;
+        //validar que el usuario exista en la bd
+        let usuarioExiste = await Usuarios.findOne({ email: data });
+        //console.log(usuarioExiste);
+        if (!usuarioExiste) {
+            return res.send({
+            estado: false,
+            mensaje: "¡Usuario no existe en la Bd!",
+            });
+        }
+        //validar credenciales
+        if(usuarioExiste && bcrypt.compareSync(req.body.password, usuarioExiste.passwordHash)){
+        //autenticacion de 2 factores con generacion del token
+            
+            const token = jwt.sign(
+                // datos a codificar en el token
+                {
+                userId: usuarioExiste.id,
+                isAdmin: usuarioExiste.esAdmin
+                },
+                //salt de la codificada o hashing o encriptado
+                seCreTo,
+                // vida util del token
+                {expiresIn:"4h"}
+            );
+            return res.send({
+                estado: true,
+                mensaje: "Ingreso exitoso al sistema",
+                token
+            });
+        }else{
+            return res.send({
+                estado: false,
+                mensaje: "¡Credenciales erroneas, intente de nuevo!",
+                error
+            });
+        }
+        //generar token
+    }
 module.exports = {
-    listartodos
+    listartodos,
+    registro,
+    login
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,18 +1,11 @@
 //rutas para consumir el modulo productos de nuestro servicio
-
 const express = require("express")
 const router = express.Router()
-
 //instanciamos el controlador correspondiente
 const usuariosCtr = require('../controllers/usuarios')
-
 //rutas que entregará el modulo producto
-
 router.get("/usuarios/listartodos", usuariosCtr.listartodos);
-router.post("/usuarios/nuevo",usuariosCtr.nuevo);
-router.get("/usuarios/buscarPorID/:id",usuariosCtr.buscarPorID);
-router.delete("/usuarios/borrarPorID/:id",usuariosCtr.borrarPorID);
-router.put("/usuarios/actualizarPorID/:id",usuariosCtr.actualizarPorID);
+router.post("/usuarios/registro",usuariosCtr.registro);
+router.put("/usuarios/login",usuariosCtr.login);
 //......
-
 module.exports = router
